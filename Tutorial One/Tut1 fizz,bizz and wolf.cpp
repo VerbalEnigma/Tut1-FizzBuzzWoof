a91196987870b200126b2a3d5335931c17f85178
Lwazi <211519616@stu.ukzn.ac.za>
2016-02-26 15:53:38 +0200

    Wolf inclusion into code

--- Tutorial One/Tut1 fizz,bizz and wolf.cpp
+++ Tutorial One/Tut1 fizz,bizz and wolf.cpp
@@ -11,6 +11,11 @@ int main()
 			cout << "Bizz" << endl;
 		else if ((i % 3) == 0 && (i % 5) == 0)
 			cout << "Fizz and Bizz" << endl;
+		//Prior to this point is the additio  of the wolf code 
+		if (i % 7 == 0 && (i % 5) != 0 && (i % 3) != 0)
+			cout << "Wolf" << endl;
+		else if ((i % 7 == 0) && (i % 3) == 0 && (i % 5) == 0)
+			cout << "Fizz , Bizz and wolf " << endl;
 		
 	}
 }
